98e7cf0fac5343ae071b6da130e569df9e217901
Daniel Pence <danieldpence6@gmail.com>
2015-04-07 15:46:34 +0000

    Add some style

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ActiveRecord::Base
-    has_attached_file :image, styles: { medium: "588x420#", small: "350x250#" },
-    default_url: "http://placehold.it/350x250"
+    has_attached_file :image, styles: { xlarge: "700x300", large: "350x350", medium: "240x172#", small: "168x120#" },
+    default_url: "http://placehold.it/240x172"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
     
     belongs_to :user
